0c7e40907b5f88a8bc98d096ba6750ca3dd601fb
Shriharsh Vinod Patil <82522510+shriharshpatil23@users.noreply.github.com>
2021-06-10 10:44:19 +0530

    all Imports

--- app/src/main/java/com/Group_No_25/Expense_Manager/fragments/BalanceCategoryFragment.java
+++ app/src/main/java/com/Group_No_25/Expense_Manager/fragments/BalanceCategoryFragment.java
@@ -13,40 +13,60 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.components.Legend;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.formatter.PercentFormatter;
-import com.github.mikephil.charting.utils.ColorTemplate;
+import android.app.DatePickerDialog;
+import android.arch.lifecycle.ViewModelProviders;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.design.widget.Snackbar;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+
 import com.Group_No_25.Expense_Manager.R;
 import com.Group_No_25.Expense_Manager.transactionDb.AppDatabase;
 import com.Group_No_25.Expense_Manager.transactionDb.AppExecutors;
+import com.Group_No_25.Expense_Manager.transactionDb.TransactionEntry;
+import com.Group_No_25.Expense_Manager.transactionDb.TransactionViewModel;
 import com.Group_No_25.Expense_Manager.utils.Constants;
-import com.Group_No_25.Expense_Manager.utils.ExpenseList;
-import java.text.DateFormat;
+
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
-import java.util.List;
-import java.util.Locale;
 
-import static com.Group_No_25.Expense_Manager.activities.MainActivity.fab;
+import android.support.design.widget.TabLayout;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v7.app.AppCompatActivity;
+
+import android.support.v4.view.ViewPager;
+import android.os.Bundle;
+import android.view.View;
+
+import com.Group_No_25.Expense_Manager.R;
+import com.Group_No_25.Expense_Manager.adapters.SectionsPageAdapter;
+import com.Group_No_25.Expense_Manager.fragments.BalanceCategoryFragment;
+import com.Group_No_25.Expense_Manager.fragments.BalanceFragment;
+import com.Group_No_25.Expense_Manager.fragments.CustomBottomSheetDialogFragment;
+import com.Group_No_25.Expense_Manager.fragments.ExpenseFragment;
+
+ import static com.Group_No_25.Expense_Manager.activities.MainActivity.fab;
 
 //public class BalanceCategoryFragment {
 //}
-public class BalanceCategoryFragment extends Fragment implements AdapterView.OnItemSelectedListener{
+public class BalanceCategoryFragment extends Fragment {
 
-    @Override
-    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
 
-    }
-
-    @Override
-    public void onNothingSelected(AdapterView<?> parent) {
-
-    }
 }
