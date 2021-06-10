dca99ab0ae478d58f7c9ab86305bb51d938fd294
Shriharsh Vinod Patil <82522510+shriharshpatil23@users.noreply.github.com>
2021-06-10 14:10:52 +0530

    Category Fragment edited

--- app/src/main/java/com/Group_No_25/Expense_Manager/fragments/BalanceCategoryFragment.java
+++ app/src/main/java/com/Group_No_25/Expense_Manager/fragments/BalanceCategoryFragment.java
@@ -1,15 +1,19 @@
 package com.Group_No_25.Expense_Manager.fragments;
+
+
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
 
@@ -67,6 +71,56 @@ import com.Group_No_25.Expense_Manager.fragments.ExpenseFragment;
 //public class BalanceCategoryFragment {
 //}
 public class BalanceCategoryFragment extends Fragment {
+
+    CardView education_card;
+    CardView food_card;
+    CardView travel_card;
+    CardView health_card;
+    CardView other_card;
+
+    EditText education_et;
+    EditText food_et;
+    EditText travel_et;
+    EditText health_et;
+    EditText other_et;
+
+    TextView education_text;
+    TextView food_text;
+    TextView travel_text;
+    TextView health_text;
+    TextView other_text;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        final View view=inflater.inflate(R.layout.balance_fragment,container,false);
+        education_card = view.findViewById(R.id.education_card_view);
+        food_card = view.findViewById(R.id.Food_card_view);
+        travel_card = view.findViewById(R.id.travel_card_view);
+        health_card = view.findViewById(R.id.health_card_view);
+        other_card = view.findViewById(R.id.other_card_view);
+
+
+        education_et = view.findViewById(R.id.Education_title_name);
+        food_et = view.findViewById(R.id.Food_title_name);
+        travel_et = view.findViewById(R.id.Travel_title_name);
+        health_et = view.findViewById(R.id.health_title_name);
+        other_et = view.findViewById(R.id.other_title_name);
+
+
+        education_text = view.findViewById(R.id.education_balance_view);
+        food_text = view.findViewById(R.id.food_balance_view);
+        travel_text = view.findViewById(R.id.travel_balance_view);
+        health_text = view.findViewById(R.id.health_balance_view);
+        other_text = view.findViewById(R.id.other_balance_view);
+
+
+
+
+
+        return view;
+    }
 
 
 }
