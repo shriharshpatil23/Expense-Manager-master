eef8d17bcd2025531e4847620fa9fb6ed37d8250
shreyas-sakri <shreyassakri9860@gmail.com>
2021-06-10 10:14:50 +0530

    Added Splash Screen

--- app/src/main/java/com/Group_No_25/Expense_Manager/activities/SplashScreen.java
+++ app/src/main/java/com/Group_No_25/Expense_Manager/activities/SplashScreen.java
@@ -1,16 +1,25 @@
 package com.Group_No_25.Expense_Manager.activities;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.Group_No_25.Expense_Manager.R;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        startActivity(new Intent(SplashScreen.this, MainActivity.class));
-        // close splash activity
-        finish();
+        setContentView(R.layout.activity_splash_screen);
+//        getSupportActionBar().hide();
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        },  3000);
+
     }
 }
